82afa5f6e0273e8e28fd01443b75a0a71c36fb6a
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-03-18 22:36:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ const person = {
   age: 80,
 };
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+// 1. Store a reference to the players array
+const team = players;
 
+// 2. Create a copy of the players array
+// Using the spread operator creates a new array with the same elements
+const team1 = [...players];
+
+// 3. Create a copy of the person object
+// This ensures cap1 is a new object, not a reference to the original
+const cap1 = { ...person };
+
+// These lines are provided in your template to expose variables to the global scope
 window.players = players;
 window.person = person;
 window.team = team;
